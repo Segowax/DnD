e415940e791f0f68ca0fd0b13c538e00ec7429eb
Segowax <kosmicznybandyta@gmail.com>
2023-11-13 12:58:56 +0100

    MediatR and AutoMapper added to the Application project.

---
+++ DnD.Application/MappingProfiles/CharacterProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace DnD.Application.MappingProfiles
+{
+    public class CharacterProfile : Profile
+    {
+        public CharacterProfile()
+        {
+            // ToDo
+        }
+    }
+}
