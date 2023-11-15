ddad88357bc535cf014c7c6345e302c02e376acb
Segowax <kosmicznybandyta@gmail.com>
2023-11-15 22:54:22 +0100

    AbilityScoresProfile added

---
+++ DnD.Application/MappingProfiles/AbilityScoresProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace DnD.Application.MappingProfiles
+{
+    public class AbilityScoresProfile : Profile
+    {
+        public AbilityScoresProfile()
+        {
+            // ToDo       
+        }
+    }
+}
