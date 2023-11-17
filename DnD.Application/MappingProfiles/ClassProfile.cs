32a58cbf5ee5a211b8437b5f839fa3e51e05fbaf
Segowax <kosmicznybandyta@gmail.com>
2023-11-17 23:23:29 +0100

    Class profile added

---
+++ DnD.Application/MappingProfiles/ClassProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace DnD.Application.MappingProfiles
+{
+    public class ClassProfile : Profile
+    {
+        public ClassProfile()
+        {
+            // ToDo  
+        }
+    }
+}
