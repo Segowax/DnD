75710653dd7df75221461837884e084cd39d4b5c
Segowax <kosmicznybandyta@gmail.com>
2023-11-20 23:49:08 +0100

    RaceProfile added

---
+++ DnD.Application/MappingProfiles/RaceProfile.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace DnD.Application.MappingProfiles
+{
+    public class RaceProfile : Profile
+    {
+        public RaceProfile()
+        {
+            // ToDo
+        }
+    }
+}
