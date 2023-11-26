5f055162ed8b0bff761880762072bd72de660a89
Segowax <kosmicznybandyta@gmail.com>
2023-11-26 11:14:17 +0100

    First command added (CreateRace)

---
+++ DnD.Application/Features/Race/Commands/CreateRace/CreateRaceCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace DnD.Application.Features.Race.Commands.CreateRace
+{
+    public class CreateRaceCommand : IRequest<Unit>
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ DnD.Application/Features/Race/Commands/CreateRace/CreateRaceHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using DnD.Application.Contracts.Persistence;
+using MediatR;
+
+namespace DnD.Application.Features.Race.Commands.CreateRace
+{
+    public class CreateRaceHandler : IRequestHandler<CreateRaceCommand, Unit>
+    {
+        private readonly IMapper _mapper;
+        private readonly IRaceRepository _raceRepository;
+
+        public CreateRaceHandler(IMapper mapper, IRaceRepository raceRepository)
+        {
+            _mapper = mapper;
+            _raceRepository = raceRepository;
+        }
+        public async Task<Unit> Handle(CreateRaceCommand request, CancellationToken cancellationToken)
+        {
+            var dataToCreate = _mapper.Map<Domain.Race>(request);
+            await _raceRepository.CreateAsync(dataToCreate, cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
