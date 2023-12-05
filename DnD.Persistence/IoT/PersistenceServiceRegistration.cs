dd4a908714c0867959e32be6526f71aead439fac
Segowax <kosmicznybandyta@gmail.com>
2023-12-05 17:54:19 +0100

    New layer introduced - persistence

---
+++ DnD.Persistence/IoT/PersistenceServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Design;
+
+namespace DnD.Persistence.IoT
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceContainer ConfigurePersistenceServices(this IServiceContainer service)
+        {
+
+
+            return service;
+        }
+    }
+}
