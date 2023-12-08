a5f20c1ea347dd3e7cc1a7560d758ef0e7bf5adf
Segowax <kosmicznybandyta@gmail.com>
2023-12-08 23:37:47 +0100

    Description configuration added

---
+++ DnD.Persistence/Configurations/DescriptionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using DnD.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DnD.Persistence.Configurations
+{
+    public class DescriptionConfiguration : IEntityTypeConfiguration<Description>
+    {
+        public void Configure(EntityTypeBuilder<Description> builder)
+        {
+
+        }
+    }
+}
